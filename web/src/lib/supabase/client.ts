3ff0c67142ea29df7a2da7dbca7e5755dd377066
Daniel Abatan <d.abatan@rivalcontent.co.uk>
2025-11-05 00:55:30 +0000

    chore(web): use createBrowserClient in supabase client

--- web/src/lib/supabase/client.ts
+++ web/src/lib/supabase/client.ts
@@ -1,9 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 
 export const supabaseBrowser = () =>
-  createClient(
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { auth: { persistSession: true, autoRefreshToken: true } }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
-
