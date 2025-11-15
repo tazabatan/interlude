3ba3421778c4c9a52e1e2f4e44b4c1f6d5d5408e
Daniel Abatan <d.abatan@rivalcontent.co.uk>
2025-11-15 20:03:52 +0000

    fix: re-export middleware config explicitly

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,1 +1,4 @@
-export { middleware as default, config } from './src/proxy'
+import { middleware, config as proxyConfig } from './src/proxy'
+
+export default middleware
+export const config = proxyConfig
